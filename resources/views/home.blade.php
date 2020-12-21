11efef85c8ca88a6e0e3b953fd4a7fc43dafb73c
Maurizio <maurizio.faedda1@gmail.com>
2020-12-21 14:14:39 +0100

    Test init project - lavarel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        <h1>hello world</h1>
+        <h3> <a href="{{ route('about') }}"> chi siamo</a> </h3>
+
+    </body>
+</html>
